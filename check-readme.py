42bb5951244f585bdae5e80c65cc06e6acab4e0d
mcjay18 <jaymcnealy@gmail.com>
2025-06-02 13:15:24 -0400

    V1.0.0

--- check-readme.py
+++ check-readme.py
@@ -5,6 +5,10 @@ import os
 import sys
 
 
+__version__ = "1.0.0"
+
+
+
 def main():
     # Define the expected README filename
     readme_files = ["README.md", "README.rst", "README.txt", "README"]
